d13805e4ffc71d3964aca6eb2465eae3d5ba845f
Roman H <ipero@live.com>
2016-03-11 23:34:09 -0600

    Add more names for random generator

--- server/modules/moduleOne.js
+++ server/modules/moduleOne.js
@@ -1,12 +1,15 @@
-firstName=["Bob","Peter","Sam","Cheesehead","Jennifer", "Adele", "Mike", "Atom", "Java", "America", "Pilot"];//11
-lastName=["Peterson", "Jackson", "Hackson", "Dudeman","Dicass", "Cockman", "Dover", "Anderson", "Andersen"];//9
+
+firstName=["Bob","Peter","Sam","Cheesehead","Jennifer", "Adele", "Mike", "Atom", "Java", "America", "Pilot",
+  "Scott"];//12
+lastName=["Peterson", "Jackson", "Hackson", "Dudeman","Codeson", "Codeman", "Scriptson", "Syberson", "Bachman",
+  "Dover", "Anderson", "Andersen"];//12
 
 var randomNumber = function(min, max){
   return Math.floor(Math.random()*(1 + max - min) + min);
 };
 
 var createName = function(){
-  var name=firstName[randomNumber(0,10)]+' '+lastName[randomNumber(0,8)];
+  var name=firstName[randomNumber(0,11)]+' '+lastName[randomNumber(0,11)];
   return name;
 }
 
